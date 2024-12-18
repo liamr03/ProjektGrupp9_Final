2e94f8956f5904aa3aef0f83619e0d35a028a720
liamr03 <liamr03@hotmail.com>
2024-12-18 10:29:01 +0100

    Added sql code from Adrian

--- schema.sql
+++ schema.sql
@@ -1,2 +1,64 @@
--- Denna fil ska innehålla SQL-kommandon för
--- att skapa databasen och tabellerna som behövs för projektet.
+CREATE
+DATABASE SkolaDB
+
+
+CREATE TABLE Studenter
+(
+    StudentID INT         NOT NULL AUTO_INCREMENT,
+    Namn      VARCHAR(50) NOT NULL,
+    Ålder     INT,
+    Kontakt   VARCHAR(100) UNIQUE,
+    Skolstart DATE,
+    BetygID   INT,
+    KursID    INT,
+    PRIMARY KEY (StudentID),
+    FOREIGN KEY (BetygID) REFERENCES Betygs (BetygsID),
+    FOREIGN KEY (KursID) REFERENCES Kurser (KursID)
+);
+
+
+CREATE TABLE Lärare
+(
+    LärarID           INT         NOT NULL AUTO_INCREMENT,
+    LärarNamn         VARCHAR(50) NOT NULL,
+    Ålder             INT,
+    Kontakt           VARCHAR(50) UNIQUE,
+    AnställningsDatum DATE,
+    KursID            INT,
+    PRIMARY KEY (LärarID),
+    FOREIGN KEY (KursID) REFERENCES Kurser (KursID)
+);
+
+
+CREATE TABLE Kurser
+(
+    KursID     INT         NOT NULL AUTO_INCREMENT,
+    Kursnamn   VARCHAR(50) NOT NULL UNIQUE,
+    Kursstart  DATE,
+    KursLärare VARCHAR(50),
+    PRIMARY KEY (KursID)
+);
+
+
+CREATE TABLE Betygs
+(
+    BetygsID INT NOT NULL AUTO_INCREMENT,
+    Betyg    INT CHECK (Betyg BETWEEN 1 AND 5),
+    LärarID  INT,
+    KursID   INT,
+    PRIMARY KEY (BetygsID),
+    FOREIGN KEY (LärarID) REFERENCES Lärare (LärarID),
+    FOREIGN KEY (KursID) REFERENCES Kurser (KursID)
+);
+
+
+CREATE TABLE KursPuls
+(
+    PulsID    INT NOT NULL AUTO_INCREMENT,
+    PulsBetyg INT CHECK (PulsBetyg BETWEEN 1 AND 5),
+    KursID    INT,
+    StudentID INT,
+    PRIMARY KEY (PulsID),
+    FOREIGN KEY (KursID) REFERENCES Kurser (KursID),
+    FOREIGN KEY (StudentID) REFERENCES Studenter (StudentID)
+);
